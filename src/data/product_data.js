5c7529c25f498486edf936e0afd96fe457a11796
Francisco Fernandes Júnior <jrfernandesti@gmail.com>
2021-01-31 11:23:27 -0300

    subindo coisas

---
+++ src/data/product_data.js
@@ -0,0 +1,26 @@
+const product_card = [
+    {
+        id:1,
+        product_name: "Jaomes Alpha",
+        description: "Hands-free, hands- on mush",
+        price: 350,
+        currency: "$",
+        thumb:"./images/1.png"
+    },{
+        id:2,
+        product_name: "Jaomes Z320",
+        description: "Hands-free, hands- on mush",
+        price: 350,
+        currency: "$",
+        thumb:"./images/1.png"
+    },{
+        id:3,
+        product_name: "Jaomes X30",
+        description: "Hands-free, hands- on mush",
+        price: 150,
+        currency: "$",
+        thumb:"./images/1.png"
+    }
+]
+
+export default product_card;
